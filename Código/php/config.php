36fffa307601e623cdc16c00063423d4bc3ef7f1
Ryan Samuel MP <101760224+Ryansmp@users.noreply.github.com>
2026-05-05 21:13:12 -0300

    Update config.php

--- Código/php/config.php
+++ Código/php/config.php
@@ -1,17 +1,12 @@
 <?php
-$host = 'localhost';
+$host = 'trolley.proxy.rlwy.net';
 $db = 'oficina360';
 $user = 'root';
-$password = '';
+$password = 'rSUcnUdLsveZesxJUOdUlcIYyNHXyzaC';
+$port = '54353';
 
 try {
-    $pdo = new PDO("mysql:host=$host", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $pdo->exec("CREATE DATABASE IF NOT EXISTS $db");
-    $pdo->exec("USE $db");
-
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
