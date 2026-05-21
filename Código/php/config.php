c00d056fb375f344b92aba5868b9b245c3888b02
Ryan Samuel <ryansamuel262@gmail.com>
2026-05-21 14:49:32 -0300

    Corrigido caminho das imagens de novo

--- Código/php/config.php
+++ Código/php/config.php
@@ -1,16 +1,27 @@
 <?php
 
-$host = 'localhost';
+$host = 'trolley.proxy.rlwy.net';
 $db = 'oficina360';
 $user = 'root';
-$password = '';
-$port = '3306';
+$password = 'rSUcnUdLsveZesxJUOdUlcIYyNHXyzaC';
+$port = '54353';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $password);
+
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $password
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    echo "Conexão realizada com sucesso!";
+
 } catch (PDOException $e) {
+
     die("Erro de conexão: " . $e->getMessage());
+
 }
+
 ?>
