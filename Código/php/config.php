fd5ab9d26697a34f98b6ea895dd46ec162bfe403
Ryan Samuel MP <101760224+Ryansmp@users.noreply.github.com>
2026-05-21 14:41:03 -0300

    Update config.php

--- Código/php/config.php
+++ Código/php/config.php
@@ -3,21 +3,25 @@
 $host = 'trolley.proxy.rlwy.net';
 $db = 'oficina360';
 $user = 'root';
-$password = 'SUA_SENHA';
+$password = 'rSUcnUdLsveZesxJUOdUlcIYyNHXyzaC';
 $port = '54353';
 
 try {
 
     $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
         $user,
         $password
     );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    echo "Conexão realizada com sucesso!";
+
 } catch (PDOException $e) {
 
     die("Erro de conexão: " . $e->getMessage());
+
 }
+
 ?>
